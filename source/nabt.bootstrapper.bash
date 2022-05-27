77a0a753b81b641d4a9ee86d3fbe15614f8688e0
Nick <nick@nicholas-morgan.com>
2022-05-26 20:19:22 -0700

    Getting rid of functions source file to fix bugs

--- source/nabt.bootstrapper.bash
+++ source/nabt.bootstrapper.bash
@@ -38,8 +38,82 @@ NABT__PATH=${NABT_PATH}
 source "${NABT__PATH}/nabt.ini.bash"
 
 
-# Source the NABT functions
-source "${NABT__PATH}/nabt.functions.bash"
+
+
+#######################################
+# Write a verbose message to stdout.
+# ARGUMENTS:
+#   Message to print
+# OUTPUTS:
+#   Writes verbose messages to stdout
+# RETURN:
+#   0
+#######################################
+nabt__message() {
+    # Only if verbose mode is turned on.
+    if [ $NABT__VERBOSE -eq 1 ]; then
+        printf "$1\n";
+    fi
+
+    NABT__RETURN_CODE=0
+    return 0
+}
+
+#######################################
+# Execute the bootstrapper.
+# ARGUMENTS:
+#   (none)
+# OUTPUTS:
+#   Writes verbose messages to stdout
+# RETURN:
+#   0 if bootstrapper succeeds; non-zero on error
+#######################################
+nabt__execute_bootstrap() {
+    figlet "Not Another: BashTools!"
+    nabt__message "Bootstrapping..."
+
+
+    # Export variables to be enabled throughout the entire environment.
+    # `bin` and `sh` directories, so those binaries/scripts may be used.
+    export PATH=$PATH:"${NABT__PATH}/bin"
+    export PATH=$PATH:"${NABT__PATH}/sh"
+    export NABT__VERBOSE=${NABT__VERBOSE}
+
+    # Load aliases.
+    alias "code=${NABT__CODE_COMMAND}"
+    alias "hosts=code /etc/hosts"
+    alias "ll=ls -lashG"
+
+    # Load utilities
+    for utility in ${NABT__UTILITIES[@]}; do
+        source "${NABT__PATH}/utilities/${utility}.bash"
+        nabt__message "\tLoaded Utility: ${utility}";
+    done
+
+    nabt__message "Bootstrapped."
+    NABT__RETURN_CODE=0
+    return 0;
+}
+
+#######################################
+# Manually run the bootstrapper.
+# ARGUMENTS:
+#   (none)
+# OUTPUTS:
+#   Writes verbose messages to stdout
+# RETURN:
+#   0 if bootstrapper succeeds; non-zero on error
+#######################################
+nabt__bootstrap() {
+#    nabt__message "Not Another BashTools! - Bootstrapping..."
+    nabt__execute_bootstrap
+
+#    if [ ${NABT__RETURN_CODE} -eq 0 ]; then
+#        nabt__message "Not Another BashTools! - Bootstrapped!"
+#    fi
+}
+
+
 
 
 
